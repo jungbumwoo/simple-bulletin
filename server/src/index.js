8c3b22b81fab7a2800746dfd576c0a567d94527d
Jungbumwoo <jungbumwoo@Naver.com>
2020-07-06 20:52:25 +0900

    0706

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import api from "./routes/api";
+import conn from "./db/mysql";
 
 const app = express();
 
 let port = 8080;
 
 app.use(bodyParser.json());
+
 app.use("/api", api);
+
+var sql = "SELECT * FROM test";
+conn.query(sql, (err, results, fields) => {
+    if (err) console.log(err);
+    console.log(results);
+});
 
 app.listen(port, () => {
     console.log("Express is listening on port", port);
